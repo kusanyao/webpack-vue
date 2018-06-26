99c3c465081a751eb9de8ededa7b8259d8c74a15
DESKTOP-TKNTM60\KEY <shaoqingzhong@shein.com>
2018-06-26 17:11:59 +0800

    init

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,10 @@
 
+
+import Vue from 'vue';
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!'
+    }
+});
--- webpack.config.js
+++ webpack.config.js
@@ -10,5 +10,10 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
+    },
+    resolve: {
+        alias: {
+            'vue': 'vue/dist/vue.js'
+        }
     }
 };

fc38a23c9f2ae369a588368c1f0f40fff81793c7
DESKTOP-TKNTM60\KEY <shaoqingzhong@shein.com>
2018-06-26 16:29:59 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+/**
+ * webpack 配置文件
+ */
+
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: path.resolve(__dirname, 'src/js/main.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    }
+};
